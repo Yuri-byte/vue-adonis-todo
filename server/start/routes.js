b2174b00b7bd23d570c4ff2fd92b058aa86975e8
Yuri Schwab <yurischwab00@gmail.com>
2021-02-17 15:23:10 -0300

    task controller and relationships

--- server/start/routes.js
+++ server/start/routes.js
@@ -25,4 +25,9 @@ Route.group(() =>{
   Route.patch('projects/:id', 'ProjectController.update').middleware('auth')
   Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth')
 
+  Route.get('projects/:id/tasks', 'TaskController.index').middleware('auth')
+  Route.post('projects/:id/tasks', 'TaskController.create').middleware('auth')
+  Route.patch('projects/tasks/:id', 'TaskController.update').middleware('auth')
+  Route.delete('projects/tasks/:id', 'TaskController.destroy').middleware('auth')
+
 }).prefix('api')
